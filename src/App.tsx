050ace0aa981abbfdcacac6e0b71f109f32c6bc1
AdithyaRodrigo <adithya.r@gkintel.com>
2026-04-19 13:25:15 +0530

    remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import NavBar from './components/Header'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+//import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useNavigate } from "react-router-dom";
+//import { useNavigate } from "react-router-dom";
 function Home() {
   
   return (
